b7e18dbd2aeb3bb534cf0becc4d3458518499e36
Ayush Raj <120264441+aaayu-07@users.noreply.github.com>
2026-02-18 22:33:13 +0530

    Time: 12 ms (60.60%) | Memory: 47.1 MB (87.93%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result=new ArrayList<>();
+        if(s.length() == 0 || words.length == 0) return result;
+
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+        int windowLen = wordLen * wordCount;
+
+        Map<String, Integer> freq=new HashMap<>();
+        for(String w:words){
+            freq.put(w,freq.getOrDefault(w,0)+1);
+        }
+        for(int i =0;i<wordLen;i++){
+            int left =i,count=0;
+            Map<String, Integer> seen=new HashMap<>();
+
+            for(int right=i;right + wordLen <= s.length();right += wordLen){
+                String word=s.substring(right,right + wordLen);
+
+                if(freq.containsKey(word)){
+                    seen.put(word, seen.getOrDefault(word, 0)+1);
+                    count++;
+
+                    while(seen.get(word) > freq.get(word)){
+                        String leftWord = s.substring(left,left + wordLen);
+                        seen.put(leftWord, seen.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+                    if(count == wordCount){
+                        result.add(left);
+                    }
+                }else{
+                    seen.clear();
+                    count=0;
+                    left = right+wordLen;
+                }
+            }
+        }
+        return result;
+    }
+}
